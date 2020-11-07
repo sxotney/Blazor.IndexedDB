912e1b0499c54be9a5f4ba34a4d6e0b7492769f2
Carl Scotney <carl@unlockeddata.com>
2020-11-07 10:35:39 +0000

    downgraded to a dotnet core 3.1 working edition

--- BlazorJsFastDataExchanger/JsFastDataExchanger.cs
+++ BlazorJsFastDataExchanger/JsFastDataExchanger.cs
@@ -1,110 +1,53 @@
-﻿using Microsoft.JSInterop;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Mono.WebAssembly.Interop;
+
 
 namespace BlazorJsFastDataExchanger
 {
     public static class JsFastDataExchanger 
     {
-        public static Action<string> OnMessage { get; set; }
-        public static Action<string> OnProgress { get; set; }
 
-        public static IJSRuntime _jsRuntime;
-        public static IJSRuntime jsRuntime
-        {
-
-            get {
-                return _jsRuntime;
-            }
-
-            set {
-
-                _jsRuntime = value;
-                _jsUnmarshalledRuntime = value as IJSUnmarshalledRuntime;
-            }
-        }
-
-
-        private static IJSUnmarshalledRuntime _jsUnmarshalledRuntime;
+        private static MonoWebAssemblyJSRuntime mono = new MonoWebAssemblyJSRuntime();
 
 
         public static void SetStringData(string variableName, string data)
         {
-           
-            _jsUnmarshalledRuntime.InvokeUnmarshalled<string, string, bool>(
-                    "BJSFDEJsFunctions.SetStringData", variableName, data);
-        }
 
-        public static string GetStringData(string variableName)
-        {
-            return _jsUnmarshalledRuntime.InvokeUnmarshalled<string, string>(
-                    "BJSFDEJsFunctions.GetStringData", variableName);
-
+            mono.InvokeUnmarshalled<string, string, bool>(
+                    "BJSFDEJsFunctions.BJSFDESetStringData", variableName, data);
         }
 
 
         public static void SetBinaryData(string variableName, byte[] data)
         {
 
-            _jsUnmarshalledRuntime.InvokeUnmarshalled<string, byte[], bool>(
-                    "BJSFDEJsFunctions.SetBinaryData", variableName, data);
+            mono.InvokeUnmarshalled<string, byte[], bool>(
+                    "BJSFDEJsFunctions.BJSFDESetBinaryData", variableName, data);
         }
 
 
-        public static async Task<bool> GetBinaryData(BJSFDEBinaryInfo binaryInfo)
+        public static string GetStringData(string variableName)
         {
-            bool b = _jsUnmarshalledRuntime.InvokeUnmarshalled<string, byte[], bool>(
-                    "BJSFDEJsFunctions.GetBinaryData", binaryInfo.variableName, binaryInfo.data);
-            await Task.Delay(1);
-            return await Task.FromResult(b);
+            return mono.InvokeUnmarshalled<string, string>(
+                    "BJSFDEJsFunctions.BJSFDEGetStringData", variableName);
+
         }
 
 
-        public static async Task<bool> GetBinaryDataByPortions(BJSFDEBinaryInfo binaryInfo)
+        public static bool GetBinaryData(string variableName, byte[] destination)
         {
+            return mono.InvokeUnmarshalled<string, byte[], bool>(
+                    "BJSFDEJsFunctions.BJSFDEGetBinaryData", variableName, destination);
 
-            bool b = _jsUnmarshalledRuntime.InvokeUnmarshalled<string, byte[], string, bool>(
-                "BJSFDEJsFunctions.GetBinaryDataChunk", binaryInfo.variableName, binaryInfo.data, binaryInfo.position + "," + binaryInfo.chunkSize);
-            await Task.Delay(1);
-            return await Task.FromResult(b);
-        }
-
-        public static bool DeleteGlobalVariable(string variableName)
-        {
-            return _jsUnmarshalledRuntime.InvokeUnmarshalled<string, bool>(
-                  "BJSFDEJsFunctions.DeleteGlobalVariable", variableName);
         }
 
         public static int GetBinaryDataLenght(string variableName)
         {
 
-            return _jsUnmarshalledRuntime.InvokeUnmarshalled<string, int>(
-                    "BJSFDEJsFunctions.GetBinaryDataLenght", variableName);
+            return mono.InvokeUnmarshalled<string, int>(
+                    "BJSFDEJsFunctions.BJSFDEGetBinaryDataLenght", variableName);
 
         }
-
-
-        [JSInvokable]
-        public static void HandleMessage(string msg)
-        {
-            OnMessage?.Invoke(msg);
-           
-        }
-
-        [JSInvokable]
-        public static void HandleProgress(string msg)
-        {
-            OnProgress?.Invoke(msg);
-
-        }
-
 
 
     }
-
-
 }
--- BlazorJsFastDataExchanger/wwwroot/BJSFDEJsInterop.js
+++ BlazorJsFastDataExchanger/wwwroot/BJSFDEJsInterop.js
@@ -2,62 +2,31 @@
 //https://github.com/dotnet/aspnetcore/blob/master/src/Components/Web.JS/src/Platform/Mono/MonoPlatform.ts
 
 window.BJSFDEJsFunctions = {
-    SetStringData: function (v, t) {
-        window[BINDING.conv_string(v)] = BINDING.conv_string(t);
+    BJSFDESetStringData: function (v, t) {
+        this[BINDING.conv_string(v)] = BINDING.conv_string(t);
         return true;
     },
-    SetBinaryData: function (v, d) {
+    BJSFDESetBinaryData: function (v, d) {
         window[BINDING.conv_string(v)] = Blazor.platform.toUint8Array(d);
         return true;
     },
-    GetStringData: function (v) {
+    BJSFDEGetStringData: function (v) {
         var variableName = BINDING.conv_string(v);
-        var result = window[variableName];
-        delete window[variableName];
+        var result = this[variableName];
+        delete this[variableName];
         return BINDING.js_to_mono_obj(result);
     },
-    GetBinaryDataLenght: function (v) {
+    BJSFDEGetBinaryDataLenght: function (v) {
         var variableName = BINDING.conv_string(v);
         return window[variableName].byteLength;
     },
-    GetBinaryData: function (v, d) {
+    BJSFDEGetBinaryData: function (v, d) {
         var variableName = BINDING.conv_string(v);
         var result = window[variableName];
         var destinationUint8Array = Blazor.platform.toUint8Array(d);
-        destinationUint8Array.set(new Uint8Array(result));
+        destinationUint8Array.set(result);
         delete window[variableName];
         return true;
     },
-    GetBinaryDataChunk: function (v, d, b) {
-        var variableName = BINDING.conv_string(v);
-        var bag = BINDING.conv_string(b);
-        var bagArray = bag.split(',');
-        var position = bagArray[0];
-        var chunkSize = bagArray[1];
-
-        var sourceUint8Array = new Uint8Array(window[variableName], position, chunkSize);
-
-        var destinationUint8Array = Blazor.platform.toUint8Array(d);
-        destinationUint8Array.set(sourceUint8Array, position);
-
-        //console.log("js " + position);
-        return true;
-    },
-    DeleteGlobalVariable: function (v)
-    { 
-        delete window[BINDING.conv_string(v)];
-        return true;
-    },
-    CallDotNetMethod: function (pAssembly, pNamespace, pClass, pMethod, pArgs) {
-        Module.mono_call_static_method("[" + pAssembly + "] " + pNamespace + "." + pClass + ":" + pMethod, [pArgs]);
-        return true;
-    },
-    InvokeOnMessageAction: function (msg) {
-        Module.mono_call_static_method("[BlazorJsFastDataExchanger] BlazorJsFastDataExchanger.JsFastDataExchanger:HandleMessage", [msg]);
-        return true;
-    },
-    InvokeOnProgressAction: function (msg) {
-        Module.mono_call_static_method("[BlazorJsFastDataExchanger] BlazorJsFastDataExchanger.JsFastDataExchanger:HandleProgress", [msg]);
-        return true;
-    },
+  
 };
